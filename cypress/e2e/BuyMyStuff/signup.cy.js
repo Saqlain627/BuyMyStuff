ed8967122e25f7213dd344ccb733340e8b7761ba
saqlain627 <saqlaina627@gmail.com>
2024-02-04 00:38:50 +0500

    Test cases of signup update

--- cypress/e2e/BuyMyStuff/signup.cy.js
+++ cypress/e2e/BuyMyStuff/signup.cy.js
@@ -20,15 +20,15 @@ describe('Signin as a buyer', {retries: 1}, function()
         cy.get('input[placeholder="Email Here*"]').type("saqlaina627@gmail.com");
         
         
-        // //User enter the password
+        //User enter the password
         cy.get('input[placeholder="Enter Password*"]').type("Saqlain@1");
         
 
-        // //user re-enter the password
+        //user re-enter the password
         cy.get('input[placeholder="Re-Enter Password*"]').type("Saqlain@1");
         
 
-        // //User add the image file
+        //User add the image file
         cy.fixture('laptop.jpg').as('imageData');
         cy.get('.uploadInner').attachFile('laptop.jpg');
 
@@ -73,15 +73,14 @@ describe('Signin as a buyer with negative test cases', {retries: 1}, function()
         cy.get('input[placeholder="Email Here*"]').type("saqlaina627gmail.com");
         
         
-        // //User enter the password
+        //User enter the password
         cy.get('input[placeholder="Enter Password*"]').type("Saqlain1");
         
-
-        // //user re-enter the password
+        //user re-enter the password
         cy.get('input[placeholder="Re-Enter Password*"]').type("Saqlain@2");
         
 
-        // //User add the image file
+        //User add the image file
         cy.fixture('laptop.jpg').as('imageData');
         cy.get('.uploadInner').attachFile('laptop.jpg');
 
@@ -125,23 +124,19 @@ describe('Sign up as a buyer sucessfully with all the positive test cases', {ret
         cy.get('input[placeholder="Email Here*"]').type("saqlaina627+302@gmail.com");
         
         
-        // //User enter the password
+        //User enter the password
         cy.get('input[placeholder="Enter Password*"]').type("Saqlain@1");
         
 
-        // //user re-enter the password
+        //user re-enter the password
         cy.get('input[placeholder="Re-Enter Password*"]').type("Saqlain@1");
         
 
-        // //User add the image file
+        //User add the image file
         cy.fixture('laptop.jpg').as('imageData');
         cy.get('.uploadInner').attachFile('laptop.jpg');
 
-       
-
         cy.get('#flexCheckDefault').check();
-
-      
 
         //User click the button
         cy.contains('button', 'Register Account').click();
